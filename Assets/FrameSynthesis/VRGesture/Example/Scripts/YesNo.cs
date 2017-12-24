7edad8d619b5e48e20048eebfae6583d6c05ab99
Brian Dean Jennings <brian@bdjnews.com>
2017-12-25 00:36:31 +1300

    Small format fix (tabs -> spaces)

--- Assets/FrameSynthesis/VRGesture/Example/Scripts/YesNo.cs
+++ Assets/FrameSynthesis/VRGesture/Example/Scripts/YesNo.cs
@@ -34,7 +34,7 @@ namespace FrameSynthesis.VR.Example
             message.transform.localPosition = new Vector3(0f, 0f, 10f);
             message.transform.localRotation = Quaternion.identity;
             message.SendMessage("SetText", text);
-			      message.transform.SetParent(null);
+            message.transform.SetParent(null);
         }
     }
 }
